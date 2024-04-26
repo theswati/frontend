5131eec2a3edafb9d52fb1b10168720169ec549e
Swati <jswati603@gmail.com>
2024-04-26 10:27:11 +0530

    frontend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect,useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 // import data from '../data';
 
+const reducer = (state, action) => {
+   console.log("state 1",state)
+   console.log("action", action)
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS': 
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+  
+};
+
+
 function HomeScreen(){
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
+    const [{ loading, error, products }, dispatch] = 
+    useReducer(reducer, {
+      products: [],
+      loading: true,
+      error: '',
+    });
+
+    console.log("all", { loading, error, products })
+    
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-       console.log(result)
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      }
+       catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
     };
     fetchData();
   }, []);
@@ -17,26 +48,33 @@ function HomeScreen(){
         <div>
         <h1>Featured Products</h1>
         <div className="products">
-          {
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          
           products.map((product) => (
             <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
+              <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
-              </a>
+              </Link>
               <div className="product-info">
-                <a href={`/product/${product.slug}`}>
+              <Link to={`/product/${product.slug}`}>
                   <p>{product.name}</p>
-                </a>
+                </Link>
                 <p>
                   <strong>${product.price}</strong>
                 </p>
                 <button>Add to cart</button>
               </div>
             </div>
-          ))}
+          ))
+        )}
         </div>
       </div>
     );
-}
+  }
+
 
 export default HomeScreen;
